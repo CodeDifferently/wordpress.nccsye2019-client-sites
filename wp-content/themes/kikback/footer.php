ef752d7eae383ef1e95761bd92106e8e337916a3
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-08-19 21:21:52 -0400

    optimized homepage

--- wp-content/themes/kikback/footer.php
+++ wp-content/themes/kikback/footer.php
@@ -7,6 +7,7 @@
   <!-- Footer contains content at the bottom of the page -->
   <footer class="bg-black small text-center text-white-50">
     <div class="container">
+      <br><br><br><br><br>
       <?php insertImage("/resources/img/kikback-logo-large-white.png", 'style="width:25%; height:25%;"'); ?>
       <br>
       Copyright &copy; The KikBack 2019
