56af10367153d825303905e2b5fa36150d5e7b3a
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-08-19 21:26:04 -0400

    optimized homepage

--- wp-content/themes/kikback/components/home-page/grand-opening-video.php
+++ wp-content/themes/kikback/components/home-page/grand-opening-video.php
@@ -1,7 +1,6 @@
-
-    <iframe
-        id="grand-opening-video"
-        src="https://www.youtube.com/embed/uVUeBOuI8Ik"
-        frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen></iframe>
+<iframe
+    id="grand-opening-video"
+    src="https://www.youtube.com/embed/uVUeBOuI8Ik"
+    frameborder="0"
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
