51f29d5a632b0089398ec6396a2158fdca77a59c
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-08-20 11:55:20 -0400

    updated front-page

--- wp-content/themes/kikback/components/front-page/body.php
+++ wp-content/themes/kikback/components/front-page/body.php
@@ -6,10 +6,10 @@
         <h1 class="mt-5">CodeDifferently Wordpress Starter Template</h1>
         <p class="lead">Complete with bootstrap-4, jquery-3, utility php functions, and responsive navigation!</p>
         <ul class="list-unstyled">
-          <li>View the <i>Home</i> page by clicking <?php insertLink("/wordpress/home-page") ?>here</a>.</li>
-          <li>View the <i>Shopping</i> page by clicking <?php insertLink("/wordpress/shop-page") ?>here</a>.</li>
-          <li>View the <i>Purchase</i> page by clicking <?php insertLink("/wordpress/purchase-page") ?>here</a>.</li>
-          <li>View the <i>About Us</i> page by clicking <?php insertLink("/wordpress/about-page") ?>here</a>.</li>
+          <li>View the <i>Home</i> page by clicking <?php insertAnchor("/wordpress/home-page") ?>here</a>.</li>
+          <li>View the <i>Shopping</i> page by clicking <?php insertAnchor("/wordpress/shop-page") ?>here</a>.</li>
+          <li>View the <i>Purchase</i> page by clicking <?php insertAnchor("/wordpress/purchase-page") ?>here</a>.</li>
+          <li>View the <i>About Us</i> page by clicking <?php insertAnchor("/wordpress/about-page") ?>here</a>.</li>
         </ul>
       </div>
     </div>
