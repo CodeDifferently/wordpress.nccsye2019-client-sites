5c91a9acc6ec8d793ddd62791bc3c7429fb09eae
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-08-25 19:21:02 -0400

    added `candy-connection` to themes; changed header to have local page references

---
+++ wp-content/themes/candy-connection/front-page.php
@@ -0,0 +1,9 @@
+<!-- my test page -->
+<?php get_header();?>
+
+<body id="page-top">
+  <?php include 'components/front-page/home-carousel.php';?>
+  <?php include 'components/front-page/body.php';?>
+</body>
+
+<?php get_footer();?>
---
+++ wp-content/themes/candy-connection/header.php
@@ -0,0 +1,26 @@
+<!-- stores head and start of html tags -->
+<!DOCTYPE html>
+<html>
+  <head><?php wp_head(); ?></head>
+
+
+
+  <!-- Navigation -->
+  <ul class="topnav" align="center">
+    <center>
+      <li><img src="./cc-logo.PNG" height="auto" width="45%" id="Logo"></li>  
+      <li><a class="active" href="..\Main-Page\index.html">Home</a></li>
+      <li><?php insertLink("/candy-connection/about")?>About</a></li>
+      <li><?php insertLink("/candy-connection/shopping/cotten-candy")?>Cotton Candy</a></li>
+      <li><?php insertLink("/candy-connection/shopping/italian-ice")?>Italian Ice</a></li>
+      <li><?php insertLink("/candy-connection/special-events")?>Special Events</a></li>
+      <li><?php insertLink("/candy-connection/shopping/cart", 'class="icon-button shopping-cart fa-lg"')?><i class="fa fa-shopping-cart"></i><span></span></a>
+      <li><a href="https://www.instagram.com/candyconnections/" align="Right" target="_blank"><i class="fa fa-instagram"></i></a>
+      <li><a href="https://www.facebook.com/CandyConnections/" align="Right" target="_blank"><i class="fa fa-facebook"></i></a>
+    </center>
+  </ul>
+
+  
+
+
+  <body <?php body_class();?> >
