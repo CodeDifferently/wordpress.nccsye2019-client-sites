88823ac78ced76654eb857c75ac3ef0a2397173b
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-08-26 12:50:50 -0400

    update

--- wp-content/themes/candy-connection/components/about-page/body.php
+++ wp-content/themes/candy-connection/components/about-page/body.php
@@ -3,25 +3,25 @@
     $name = "Person Name";
     $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.";
     $link = "https://google.com";
-    include('_person-card.php') ?>
+    include('_product.php') ?>
 
 <?php 
     $img = "/resources/img/placeholder-700x400.png";
     $name = "Person Name";
     $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.";
     $link = "https://google.com";
-    include('_person-card.php') ?>
+    include('_product.php') ?>
 
 <?php 
     $img = "/resources/img/placeholder-700x400.png";
     $name = "Person Name";
     $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.";
     $link = "https://google.com";
-    include('_person-card.php') ?>
+    include('_product.php') ?>
 
 <?php   
     $img = "/resources/img/placeholder-700x400.png";
     $name = "Person Name";
     $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.";
     $link = "https://google.com";
-    include('_person-card.php') ?>
+    include('_product.php') ?>
--- wp-content/themes/candy-connection/components/cotton-candy-page/body.php
+++ wp-content/themes/candy-connection/components/cotton-candy-page/body.php
@@ -1,156 +1,57 @@
 <!-- Page Content -->
 <!-- /.col-lg-3 -->
-<center>
-  <div class="col-lg-9">
+<?php include('carousel.php') ?>
 
-    <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner" role="listbox">
-        <div class="carousel-item active">
-          <img class="d-block img-fluid" src=./img/pops.jpg alt="First slide">
-        </div>
-        <div class="carousel-item">
-          <img class="d-block img-fluid" src="https://images.vat19.com/covers/large/pancakes-maple-syrup-cotton-candy.jpg"
-            alt="Second slide">
-        </div>
-        <div class="carousel-item">
-          <img class="d-block img-fluid" src="https://images-na.ssl-images-amazon.com/images/I/41VEE3F8TyL._SY450_.jpg"
-            alt="Third slide">
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-</center>
+
 <div class="row">
+  <?php 
+      $img = "/resources/img/placeholder-700x400.png";
+      $name = "Person Name";
+      $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.";
+      $link = "https://google.com";
+      include('_product.php') ?>
 
-  <div class="col-lg-4 col-md-6 mb-4">
-    <div class="card h-100">
-      <a href="#"><img class="card-img-top" src=./img/strawberry.jpg alt="strawberry"></a>
-      <div class="card-body">
-        <h4 class="card-title">
-          <a href="#">strawberry</a>
-        </h4>
-        <h5>Small $3.00</h5>
-        <h4>Medium $5.00</h4>
-        <h4>Large $7.00</h4>
-        <p class="card-text"></p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-      </div>
-    </div>
-  </div>
+  <?php 
+      $img = "/resources/img/placeholder-700x400.png";
+      $name = "Person Name";
+      $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.";
+      $link = "https://google.com";
+      include('_product.php') ?>
 
-  <div class="col-lg-4 col-md-6 mb-4">
-    <div class="card h-100">
-      <img src=./img/chee.jpg alt="chee" width="615px" height="700px">
-      <div class="card-body">
-        <h4 class="card-title">
-          <a href="#">cherry</a>
-        </h4>
-        <h5>Small $3.00</h5>
-        <h5>Medium $5.00</h5>
-        <h5>Large $7.00</h5>
-        <p class="card-text"></p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-      </div>
-    </div>
-  </div>
+  <?php 
+      $img = "/resources/img/placeholder-700x400.png";
+      $name = "Person Name";
+      $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.";
+      $link = "https://google.com";
+      include('_product.php') ?>
+</div>
 
-  <div class="col-lg-4 col-md-6 mb-4">
-    <div class="card h-100">
-      <a href="#"><img class="card-img-top" src=./img/orange.jpg alt="orange"></a>
-      <div class="card-body">
-        <h4 class="card-title">
-          <a href="#">Orange creamsicle</a>
-        </h4>
-        <h5>Small $3.00</h5>
-        <h5>Medium $5.00</h5>
-        <h5>Large $7.00</h5>
-        <p class="card-text"></p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-      </div>
-    </div>
-  </div>
 
-  <div class="col-lg-4 col-md-6 mb-4">
-    <div class="card h-100">
-      <a href="#"><img class="card-img-top" src=./img/grape.jpg alt="grape"></a>
-      <div class="card-body">
-        <h4 class="card-title">
-          <a href="#">Grape soda</a>
-        </h4>
-        <h5>Small $3.00</h5>
-        <h5>Medium $5.00</h5>
-        <h5>Large $7.00</h5>
-        <p class="card-text"></p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-      </div>
-    </div>
-  </div>
 
-  <div class="col-lg-4 col-md-6 mb-4">
-    <div class="card h-100">
-      <a href="#"><img class="card-img-top" src=./img/lemonade.jpg alt="lemonade"></a>
-      <div class="card-body">
-        <h4 class="card-title">
-          <a href="#">Lemonade</a>
-        </h4>
-        <h5>Small $3.00</h5>
-        <h5>Medium $5.00</h5>
-        <h5>Large $7.00</h5>
-        <p class="card-text">.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-      </div>
-    </div>
-  </div>
 
-  <div class="col-lg-4 col-md-6 mb-4">
-    <div class="card h-100">
-      <a href="#"><img class="card-img-top" src="https://mail.google.com/mail/u/0?ui=2&ik=1fdeafff6f&attid=0.1&permmsgid=msg-f:1641776450379432748&th=16c8c2fbad77c32c&view=fimg&sz=s0-l75-ft&attbid=ANGjdJ_bQcYvAn2eintbn7Zm2YBjJK-SkiuPfu9vRyUx6w2UZL2WAocaYKWoNFWUotIOjjKZda_c_Zl8TRpEELpSCqJvclj2otKFtnll7EX0uVDv86xRmvPDAyXIT4A&disp=emb&realattid=e9ed0b0534e4abbf_0.1.1"
-          alt=""></a>
-      <div class="card-body">
-        <h4 class="card-title">
-          <a href="#">Unicorn birthday cake</a>
-        </h4>
-        <h5>Small $3.00</h5>
-        <h5>Medium $5.00</h5>
-        <h5>Large $7.00</h5>
-        <p class="card-text"></p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-      </div>
-    </div>
-  </div>
 
+
+<div class="row">
+  <?php   
+      $img = "/resources/img/placeholder-700x400.png";
+      $name = "Person Name";
+      $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.";
+      $link = "https://google.com";
+      include('_product.php') ?>
+
+
+  <?php 
+      $img = "/resources/img/placeholder-700x400.png";
+      $name = "Person Name";
+      $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.";
+      $link = "https://google.com";
+      include('_product.php') ?>
+
+  <?php   
+      $img = "/resources/img/placeholder-700x400.png";
+      $name = "Person Name";
+      $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.";
+      $link = "https://google.com";
+      include('_product.php') ?>
 </div>
 <!-- /.row -->
-
-</div>
-<!-- /.col-lg-9 -->
-
-</div>
-<!-- /.row -->
-
-</div>
-<!-- /.container -->
